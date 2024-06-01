a1bdd243e479b65500e0571bb914fcb574df1051
Omartzrt <luis776891@gmail.com>
2024-05-31 22:39:51 -0600

    ProyectoBack V2

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import mongoose from 'mongoose';
 import usuarioRoute from "./routes/usuarioRoute";
 import productoRoute from "./routes/productoRoute";
+import {Request, Response} from "express";
 
 mongoose.connect(process.env.DB_CONNECTION_STRING as string)
     .then(() => {
@@ -13,6 +14,11 @@ mongoose.connect(process.env.DB_CONNECTION_STRING as string)
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'))
+
+app.get('/health', async(req: Request, res:Response) => {
+    res.send({message: "Servidor OK!!"})
+})
 
 app.use('/api/users/', usuarioRoute);
 app.use('/api/products/', productoRoute);
@@ -21,3 +27,7 @@ app.use('/api/products/', productoRoute);
 app.listen(3000, () =>{
     console.log("App corriendo en el puerto 3000");
 })
+
+function morgan(arg0: string): any {
+    throw new Error('Function not implemented.');
+}
